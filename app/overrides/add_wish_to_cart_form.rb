a52ae430b0696b6949a65aae4a143bece08e9025
Christian Finck <christian@finck.at>
2017-01-26 16:34:30 +0100

    moved stuff from partial to override

--- app/overrides/add_wish_to_cart_form.rb
+++ app/overrides/add_wish_to_cart_form.rb
@@ -1,6 +1,13 @@
 Deface::Override.new(
-  virtual_path: 'spree/products/show',
-  name: 'add_wish_to_cart_form',
-  insert_bottom: "[data-hook='cart_form']",
-  text: "<%= render partial: 'spree/products/wishlist_form', locals: { product: @product } %>",
+    virtual_path: 'spree/products/show',
+    name: 'add_wish_to_cart_form',
+    insert_bottom: "[data-hook='cart_form']",
+    text: "
+    <% if spree_current_user %>
+      <div class='row'>
+        <div id='wishlist-form' class='col-md-5 col-md-offset-6 text-right'>
+          <%= render partial: 'spree/products/wishlist_form', locals: {product: @product, button: '', button_only: false} %>
+        </div>
+      </div>
+    <% end %>",
 )
