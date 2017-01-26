b8f33c441a1cb9ff2b14541a09cc7de4a6a1bf69
Christian Finck <christian@finck.at>
2017-01-26 16:07:05 +0100

    changed partial to use local var instead of instance var

--- app/overrides/add_wish_to_cart_form.rb
+++ app/overrides/add_wish_to_cart_form.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'add_wish_to_cart_form',
   insert_bottom: "[data-hook='cart_form']",
-  partial: 'spree/products/wishlist_form'
+  text: "<%= render partial: 'spree/products/wishlist_form', locals: { product: @product } %>",
 )
