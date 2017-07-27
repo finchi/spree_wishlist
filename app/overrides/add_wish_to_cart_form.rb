8920540dd7aa70bb165b1b370e887804a4559680
Christian Finck <christian@finck.at>
2017-07-27 15:43:56 +0200

    refactored version bump

--- app/overrides/add_wish_to_cart_form.rb
+++ app/overrides/add_wish_to_cart_form.rb
@@ -3,13 +3,13 @@ Deface::Override.new(
     name: 'add_wish_to_cart_form',
     insert_bottom: "[data-hook='cart_form']",
     text: "
-    <% if spree_current_user %>
-      <div class='row'>
-        <div id='wishlist-form' class='col-md-5 col-md-offset-6 text-right'>
+    <div class='row'>
+      <div id='wishlist-form' class='col-md-5 col-md-offset-6 text-right'>
+        <% if spree_current_user %>
           <%= render partial: 'spree/products/wishlist_form', locals: {product: @product, button: '', button_only: false} %>
-        </div>
+        <% else %>
+          <%= link_to 'Back', request.referer.present? ? request.referer : prodcuts_path, class: 'btn btn-default' %>
+        <% end %>
       </div>
-    <% else %>
-      <%= link_to 'Back', request.referer.present? ? request.referer : prodcuts_path, class: 'btn btn-default' %>
-    <% end %>",
+    </div>",
 )
